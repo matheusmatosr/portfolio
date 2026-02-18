617118649779866626ff596e7b0872de4665304e
Matheus Matos <matheusmatosrodrigues27@gmail.com>
2026-02-18 20:09:12 -0300

    clear

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,11 +1,9 @@
 const changeThemeBtn = document.querySelector("#change-theme");
 
-// Toggle dark mode
 function toggleDarkMode() {
   document.body.classList.toggle("dark");
 }
 
-// Load light or dark mode
 function loadTheme() {
   const darkMode = localStorage.getItem("dark");
 
